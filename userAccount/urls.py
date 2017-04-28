a6e01ae5d497c152ce0e57b1e1c935e9ede5c07e
Sim SangHyun <ipod1597@naver.com>
2017-04-29 01:50:37 +0900

    register login logout

---
+++ userAccount/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^sign/$', views.SignForm.as_view(), name='signForm'),
+    url(r'^login/$', views.LoginForm.as_view(), name='loginForm'),
+    url(r'^logout/$', views.logout, name='logout'),
+]
--- userAccount/views.py
+++ userAccount/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout as _logout
 from .models import MyUser
+
+class MainForm(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'main.html')
+
+    def post(self, request, *args, **kwargs):
+        pass
 
 class SignForm(View):
     def get(self, request, *args, **kwargs):
-        pass
+        return render(request, 'userAccount/signForm.html')
 
     def post(self, request, *args, **kwargs):
         user_id = request.POST['user_id']
         user_pw = request.POST['user_pw']
-        user_email = request.POST['user_email']
 
         try:
-            user_model = MyUser.objects.create(username=user_id, email=user_email)
+            user_model = MyUser.objects.create(username=user_id)
             user_model.set_password(user_pw)
             user_model.save()
         except:
-            return HttpResponse(status=204)
+            return render(request, 'userAccount/signForm.html', {'error' : "회원가입에 실패하였습니다."})
 
-        return HttpResponse(status=200)
+        return render(request, 'userAccount/loginForm.html', {'msg' : "환영합니다."})
+
+class LoginForm(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'userAccount/loginForm.html')
+
+    def post(self, request, *args, **kwargs):
+        user_id = request.POST['user_id']
+        user_pw = request.POST['user_pw']
+        user = authenticate(username=user_id, password=user_pw)
+        if user is not None:
+            login(request, user)
+            return render(request, 'main.html')
+        else:
+            return render(request, 'userAccount/loginForm.html', {'msg' : "로그인에 실패하였습니다."})
+
+def logout(request):
+    _logout(request)
+    return render(request, 'main.html')
